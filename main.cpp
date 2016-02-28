a4812f44e06564a198a808e17744e6e1de5db841
andigor <amotsok@gmail.com>
2016-02-28 19:40:11 +0200

    cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <boost/functional/hash.hpp>
+#include <boost/array.hpp>
+
+
+int main()
+{
+
+  return 0;
+}
